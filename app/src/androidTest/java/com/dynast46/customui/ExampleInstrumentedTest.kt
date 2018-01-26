eb6d3e7e6b8c957e22b526600cdd2251a2a4a3ae
sweetylove2k <changgu_kim@nextapps.com>
2018-01-26 14:54:30 +0900

    Initial commit

---
+++ app/src/androidTest/java/com/dynast46/customui/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.dynast46.customui
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.dynast46.customui", appContext.packageName)
+    }
+}
